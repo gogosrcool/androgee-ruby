9eb1f61d9c376e8fb785280d975006a04750733c
egee-irl <brian@egee.io>
2017-10-26 07:06:09 -0700

    bug fixes

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 docker build . -t discord
 
 git clone 
-mkdir wrcon && cd wrcon
 git clone https://github.com/egee-irl/andro-wrcon.git
+cd andro-wrcon
 docker build . -t wrcon
 
 cd .. && docker-compose up
