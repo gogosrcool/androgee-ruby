a4cc90c63c0dc4478115c085ab2ae4847bb84d9c
egee-irl <brian@egee.io>
2017-10-26 07:13:13 -0700

    this _should_ work

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ docker build . -t discord
 git clone 
 git clone https://github.com/egee-irl/andro-wrcon.git
 cd andro-wrcon
+sed -i s/localhost/redis/g /main.coffee
 docker build . -t wrcon
 
 cd .. && docker-compose up
