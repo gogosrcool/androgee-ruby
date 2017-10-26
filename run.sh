3c8a597ec793e2fb6e23035f42614208f667fdb5
egee-irl <brian@egee.io>
2017-10-26 08:58:14 -0700

    sed envars

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+sed -i s/_DISCORDTOKEN_/$DISCORD_TOKEN/g dockerfile
 docker build . -t discord
 
 git clone 
 git clone https://github.com/egee-irl/andro-wrcon.git
 cd andro-wrcon
 sed -i s/localhost/redis/g main.coffee
+
+sed -i s/_RUSTIP_/$RUST_IP/g dockerfile
+sed -i s/_RUSTPORT_/$RUST_PORT/g dockerfile
+sed -i s/_RUSTPASSWORD_/$RUST_PASSWORD/g dockerfile
+
 docker build . -t wrcon
 
 cd .. && docker-compose up
