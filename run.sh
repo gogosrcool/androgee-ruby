0fdb007eb30b8efdacacd560274b27ddf2e43a35
egee-irl <brian@egee.io>
2017-10-27 07:05:56 -0700

    not sure about this

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ sed -i s/_RUSTIP_/$RUST_IP/g dockerfile
 sed -i s/_RUSTPASSWORD_/$RUST_PASSWORD/g dockerfile
 docker build . -t discord
 
-git clone
 git clone https://github.com/egee-irl/andro-wrcon.git
 cd andro-wrcon
 sed -i s/localhost/redis/g main.coffee
