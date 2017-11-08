8c99027a07b471014fc5f866771428f444020365
egee-irl <brian@egee.io>
2017-11-08 06:08:14 -0800

    update transform script

---
+++ transform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i s/_DISCORDTOKEN_/$DISCORD_TOKEN/ dockerfile
+sed -i s/_RUSTIP_/$RUST_IP/ dockerfile
+sed -i s/_RUSTPASSWORD_/$RUST_PASSWORD/ dockerfile
+sed -i s/_EGEEIOSERVER_/$EGEEIO_SERVER/ dockerfile
