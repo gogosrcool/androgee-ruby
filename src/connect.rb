a97c63830e3958c421b0a2164dc0ae40799526b3
egee-irl <brian@egee.io>
2018-12-27 22:50:38 -0800

    welp

--- src/connect.rb
+++ src/connect.rb
@@ -3,6 +3,6 @@ require 'discordrb'
 # Do it
 class Connect
   def discord
-    Discordrb::Commands::CommandBot.new(token: 'NTEwNzIwMzIyMjI4NzE1NTI0.DsgdrQ.MIPET-VFYCUR8oF8wyAzrR_5HZU', prefix: '|')
+    Discordrb::Commands::CommandBot.new(token: '', prefix: '|')
   end
 end
