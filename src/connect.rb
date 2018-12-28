fafcdb9a0357ffa9b774dd69a2499fe814a61ce6
egee-irl <brian@egee.io>
2018-12-27 22:52:52 -0800

    Much better

--- src/connect.rb
+++ src/connect.rb
@@ -3,6 +3,6 @@ require 'discordrb'
 # Do it
 class Connect
   def discord
-    Discordrb::Commands::CommandBot.new(token: '', prefix: '|')
+    Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'], prefix: '|')
   end
 end
