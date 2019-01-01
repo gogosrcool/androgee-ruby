05f25db1c391091872e3d21ddbd057b275cc8838
egee-irl <brian@egee.io>
2019-01-01 03:23:16 -0800

    prefix as envar

--- src/connect.rb
+++ src/connect.rb
@@ -3,6 +3,6 @@ require 'discordrb'
 # Do it
 class Connect
   def discord
-    Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'], prefix: '|')
+    Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'], prefix: ENV['PREFIX'])
   end
 end
