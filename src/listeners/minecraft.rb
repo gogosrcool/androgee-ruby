692a02ad6a0d378d2f6941de7073fe4cd41496a7
egee-irl <brian@egee.io>
2019-01-02 20:55:28 -0800

    linted

--- src/listeners/minecraft.rb
+++ src/listeners/minecraft.rb
@@ -7,7 +7,8 @@ class MinecraftListener
     @get_container = GetContainer.new
     @discord_helpers = DiscordHelpers.new(discord)
   end
-  def Listen
+
+  def listen
     container = @get_container.get_em('gscminecraft_minecraft-server_1')
     player_regex = /(?<=\bUUID\sof\splayer\s)(\w+)/
     @discord_helpers.game_announce(container, player_regex, 'debug')
