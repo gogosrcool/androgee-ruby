1fb3803e7fa06159c53b4f1eef1c4b764cf82a4a
egee-irl <brian@egee.io>
2019-01-02 23:43:45 -0800

    broke up constructor

--- src/listen.rb
+++ src/listen.rb
@@ -1,22 +1,28 @@
 require 'docker'
 require 'discordrb'
 require './listeners/minecraft'
-require './commands/get_container'
 
 # I'm listening
 class Listen
   def initialize
-    timers = Timers::Group.new
-    bot = Discordrb::Commands::CommandBot
-          .new(token: ENV['TOKEN'], prefix: ENV['PREFIX'])
-    bot.ready do
-      minecraft_listener = MinecraftListener.new(bot)
-      timers.now_and_every(30) do
-        minecraft_listener.listen
-      end
-      Thread.new { loop { timers.wait } }
-      puts 'I\'m in'
+    @timers = Timers::Group.new
+    @bot = Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'], prefix: ENV['PREFIX'])
+    discord
+  end
+
+  def discord
+    @bot.ready do
+      puts 'I\'m listening'
+      game_loop
     end
-    bot.run
+    @bot.run
+  end
+
+  def game_loop
+    minecraft_listener = MinecraftListener.new(@bot)
+    @timers.now_and_every(30) do
+      minecraft_listener.listen
+    end
+    Thread.new { loop { @timers.wait } }
   end
 end
