670200fc28e070ca217fe2fef8a862838fbd2ba8
egee-irl <brian@egee.io>
2019-01-05 23:43:10 -0800

    modularized and simplifed

--- src/listeners/minecraft.rb
+++ src/listeners/minecraft.rb
@@ -2,14 +2,11 @@ require './helpers/discord'
 require './helpers/container'
 
 # Comment
-class MinecraftListener
-  def initialize(discord)
-    @discord_helpers = DiscordHelpers.new(discord)
-  end
-
-  def listen
-    container = Container.get_container('gscminecraft_minecraft-server_1')
+module MinecraftListener
+  def self.listen(bot)
+    channel = DiscordHelpers.discord_channel(bot.servers.dig(ENV['SERVER_ID'].to_i), 'debug')
     player_regex = /(?<=\bUUID\sof\splayer\s)(\w+)/
-    @discord_helpers.game_announce(container, player_regex, 'debug')
+    container = Container.get_container(ENV['MINECRAFT_NAME'])
+    DiscordHelpers.game_announce(container, player_regex, channel)
   end
 end
